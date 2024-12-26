65215a5fafd3fcbb453e681b0a76a373ced4a741
Szymon Tomasiak <sz.tomasiak@gmail.com>
2024-12-26 22:01:43 +0100

    Fix looping through dictionary

--- save_to_file.py
+++ save_to_file.py
@@ -1,6 +1,6 @@
 def save_to_file(file_name, msg_arr):
     with open(file_name, 'a+') as f:
         for dictionary in msg_arr:
-            for key, value in dictionary:
+            for key, value in dictionary.items():
                 f.write(f"{key}: {value}\n")
             f.write('\n\n')
