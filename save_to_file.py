06b991bdf900e1afef5c9e93c07048ef180cb93c
Szymon Tomasiak <sz.tomasiak@gmail.com>
2024-12-26 21:22:12 +0100

    Add functionality for saving messages to the file

--- save_to_file.py
+++ save_to_file.py
@@ -1,2 +1,7 @@
-def save_to_file():
-
+def save_to_file(msg_arr):
+    file_name = "messages.txt"
+    with open(file_name, 'a+') as f:
+        for internal_arr in msg_arr:
+            for message in internal_arr:
+                f.write(message)
+            f.write('\n')
