851dc1cfc48423adac797c9cc95883192a12086e
Szymon Tomasiak <sz.tomasiak@gmail.com>
2024-12-26 21:42:10 +0100

    Sligthly modify function

--- append_messages.py
+++ append_messages.py
@@ -1,8 +1,8 @@
 def append_messages(txt_msg, code_msg, arr: [], mode: int):
     match mode:
         case 1:
-            msgs_dict = {'Text message: ': txt_msg, 'Code message: ': code_msg}
+            msgs_dict = {'Text message': txt_msg, 'Code message: ': code_msg}
             arr.append(msgs_dict)
         case 2:
-            msgs_dict = {'Code message: ': code_msg, 'Text message': txt_msg}
+            msgs_dict = {'Code message': code_msg, 'Text message': txt_msg}
             arr.append(msgs_dict)
