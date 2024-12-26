2084a0b574e34e6284daf31974b242fee98d02be
Szymon Tomasiak <sz.tomasiak@gmail.com>
2024-12-26 21:45:00 +0100

    Modify function to loop through dictionary

--- save_to_file.py
+++ save_to_file.py
@@ -1,6 +1,6 @@
 def save_to_file(file_name, msg_arr):
     with open(file_name, 'a+') as f:
-        for internal_arr in msg_arr:
-            for message in internal_arr:
-                f.write(message)
-            f.write('\n')
+        for dictionary in msg_arr:
+            for key, value in dictionary:
+                f.write(f"{key}: {value}\n")
+            f.write('\n\n')
