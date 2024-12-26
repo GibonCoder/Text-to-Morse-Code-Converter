615481440357c15da0e88620b53504486101ddb7
Szymon Tomasiak <sz.tomasiak@gmail.com>
2024-12-26 21:34:22 +0100

    Create function that appends messages dictionary to the list

---
+++ append_messages.py
@@ -0,0 +1,8 @@
+def append_messages(txt_msg, code_msg, arr: [], mode):
+    match mode:
+        case 1:
+            msgs_dict = {'Text message: ': txt_msg, 'Code message: ': code_msg}
+            arr.append(msgs_dict)
+        case 2:
+            msgs_dict = {'Code message: ': code_msg, 'Text message': txt_msg}
+            arr.append(msgs_dict)
