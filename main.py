045cd67ff0c7c9355902103c679942f0f37c975f
Szymon Tomasiak <sz.tomasiak@gmail.com>
2024-12-28 17:48:03 +0100

    Validate user input.

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ from interface import Interface
 interface = Interface()
 
 while True:
-    action = int(input(Fore.MAGENTA + """What you want to do?
-            1 - Convert text to morse code
-            2 - Convert morse code to text (please remember to separate each character with space and each word with /)
-            3 - Import your messages
-            0 - Exit program
-            
-            Action: """ + Style.RESET_ALL))
-    if not interface.choose_action(action):
-        break
+    try:
+        action = int(input(Fore.MAGENTA + """What you want to do?
+                1 - Convert text to morse code
+                2 - Convert morse code to text (please remember to separate each character with space and each word with /)
+                3 - Import your messages
+                0 - Exit program
+                
+                Action: """ + Style.RESET_ALL))
+    except ValueError:
+        print('Invalid input. Please enter a number.')
+    else:
+        if not interface.choose_action(action):
+            break
