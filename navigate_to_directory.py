5d00473f7d6d5ea3e6e70c64546ed2dc1ccfd8a5
Szymon Tomasiak <sz.tomasiak@gmail.com>
2024-12-28 22:01:37 +0100

    Fix bug. Add colors to displayed text

--- navigate_to_directory.py
+++ navigate_to_directory.py
@@ -1,12 +1,13 @@
 import os
+from colorama import Fore, Style
 
 
 def navigate_to_directory(start_path='.'):
-    print('Please select directory where you would like to save the file.')
+    print(Fore.CYAN + 'Please select directory where you would like to save the file.' + Style.RESET_ALL)
     current_path = os.path.abspath(start_path)
 
     while True:
-        print(f'\nCurrent Directory: {current_path}')
+        print(Fore.CYAN + f'\nCurrent Directory: {current_path}' + Style.RESET_ALL)
         print('Select an option:')
 
         # List directories and files
@@ -14,26 +15,24 @@ def navigate_to_directory(start_path='.'):
         directories = [d for d in items if os.path.isdir(os.path.join(current_path, d))]
 
         for i, directory in enumerate(directories, start=1):
-            print(f'\n{i}. {directory}')
-            print('0: Select this directory')
-            print('-1: Go up one level')
-            print('Position of a specific directory inside current directory: to the subdirectory')
+            print(Fore.LIGHTGREEN_EX + f'\n{i}. {directory}' + Style.RESET_ALL)
+        print(Fore.YELLOW + '0: Select this directory' + Style.RESET_ALL)
+        print(Fore.YELLOW + '-1: Go up one level' + Style.RESET_ALL)
+        print(Fore.YELLOW + 'Position of a specific directory inside current directory: to the subdirectory' + Style.RESET_ALL)
 
-            try:
-                choice = int(input('\nEnter your choice: '))
-                if choice == 0:
-                    return current_path
-                elif choice == -1:
-                    parent_path = os.path.dirname(current_path)
-                    if parent_path == current_path:
-                        print('You are already in the root directory.')
-                    else:
-                        current_path = parent_path
-                elif 1 <= choice <= len(directories):
-                    current_path = os.path.join(current_path, directories[choice - 1])
+        try:
+            choice = int(input(Fore.GREEN + '\nEnter your choice: ' + Style.RESET_ALL))
+            if choice == 0:
+                return current_path
+            elif choice == -1:
+                parent_path = os.path.dirname(current_path)
+                if parent_path == current_path:
+                    print(Fore.BLUE + 'You are already in the root directory.' + Style.RESET_ALL)
                 else:
-                    print('Invalid choice. Please try again.')
-            except ValueError:
-                print('Invalid input. Please enter a number.')
-
-# TODO: Work more on function to fix the bug
+                    current_path = parent_path
+            elif 1 <= choice <= len(directories):
+                current_path = os.path.join(current_path, directories[choice - 1])
+            else:
+                print(Fore.RED + 'Invalid choice. Please try again.' + Style.RESET_ALL)
+        except ValueError:
+            print(Fore.RED + 'Invalid input. Please enter a number.' + Style.RESET_ALL)
