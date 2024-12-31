84d34f34d0712c14f5d1a386882aa04feeb55a06
Szymon Tomasiak <sz.tomasiak@gmail.com>
2024-12-31 14:23:11 +0100

    Create test file and write tests for append_messages function)

---
+++ test_append_messages.py
@@ -0,0 +1,26 @@
+import unittest
+from append_messages import append_messages
+
+
+class TestAppendMessages(unittest.TestCase):
+    def test_mode_1(self):
+        arr = []
+        append_messages('Hello', '.... . .-.. .-.. ---', arr, mode=1)
+        expected = [{'Text message: ': 'Hello', 'Code message: ': '.... . .-.. .-.. ---'}]
+        self.assertEqual(arr, expected)
+
+    def test_mode_2(self):
+        arr = []
+        append_messages('Hello', '.... . .-.. .-.. ---', arr, mode=2)
+        expected = [{'Code message: ': '.... . .-.. .-.. ---', 'Text message: ': 'Hello'}]
+        self.assertEqual(arr, expected)
+
+    def test_invalid_mode(self):
+        arr = []
+        append_messages('Hello', '.... . .-.. .-.. ---', arr, mode=3)
+        expected = []
+        self.assertEqual(arr, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
