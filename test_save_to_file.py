dd40c8944ce37eb5efa6221a566ff7208e6380b8
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-01-02 22:51:49 +0100

    Remove unnecessary indentation from test_save_fo_file_exception function. Finish up test

--- test_save_to_file.py
+++ test_save_to_file.py
@@ -24,20 +24,20 @@ class TestSaveToFile(unittest.TestCase):
         handle.write.assert_any_call('Code message: .... . .-.. .-.. ---\n')
         handle.write.assert_any_call('\n\n')
 
-        @patch('text_to_morse_code.save_to_file.navigate_to_directory', return_value='/mocked/directory')
-        @patch('text_to_morse_code.save_to_file.define_file_name', return_value='output.txt')
-        @patch('builtins.open', side_effect=Exception("Mocked exception"))
-        def test_save_to_file_exception(self,
-                                        mock_open_func,
-                                        mock_define_file_name,
-                                        mock_navigate_to_directory):
-            # Mock input data
-            test_msg_arr = [{'Text message: ': 'Hello', 'Code message: ': '.... . .-.. .-.. ---'}]
+    @patch('text_to_morse_code.save_to_file.navigate_to_directory', return_value='/mocked/directory')
+    @patch('text_to_morse_code.save_to_file.define_file_name', return_value='output.txt')
+    @patch('builtins.open', side_effect=Exception("Mocked exception"))
+    def test_save_to_file_exception(self,
+                                    mock_open_func,
+                                    mock_define_file_name,
+                                    mock_navigate_to_directory):
+        # Mock input data
+        test_msg_arr = [{'Text message: ': 'Hello', 'Code message: ': '.... . .-.. .-.. ---'}]
 
-            # Call the function and ensure no exceptions propagate
-            with self.assertLogs() as log:
-                save_to_file(test_msg_arr)
-                self.assertIn('Error saving file', log.output[0])
+        # Call the function and ensure no exceptions propagate
+        with self.assertLogs() as log:
+            save_to_file(test_msg_arr)
+            self.assertIn('Error saving file', log.output[0])
 
     @patch('builtins.input', return_value='test.txt')
     def test_define_file_name(self, mock_input):
@@ -46,3 +46,5 @@ class TestSaveToFile(unittest.TestCase):
         mock_input.assert_called_once_with('\033[34mEnter a filename (e.g., output.txt). It can be an existing one, or you can name a fresh new file: \033[0m')
 
 
+if __name__ == '__main':
+    unittest.main()
