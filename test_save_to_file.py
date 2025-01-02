101172090062a348b9fd2b04a6c1b95d21816679
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-01-02 22:31:25 +0100

    Write functionality to test exception

--- test_save_to_file.py
+++ test_save_to_file.py
@@ -23,3 +23,18 @@ class TestSaveToFile(unittest.TestCase):
         handle.write.assert_any_call('Text message: Hello\n')
         handle.write.assert_any_call('Code message: .... . .-.. .-.. ---\n')
         handle.write.assert_any_call('\n\n')
+
+        @patch('text_to_morse_code.save_to_file.navigate_to_directory', return_value='/mocked/directory')
+        @patch('text_to_morse_code.save_to_file.define_file_name', return_value='output.txt')
+        @patch('builtins.open', side_effect=Exception("Mocked exception"))
+        def test_save_to_file_exception(self,
+                                        mock_open_func,
+                                        mock_define_file_name,
+                                        mock_navigate_to_directory):
+            # Mock input data
+            test_msg_arr = [{'Text message: ': 'Hello', 'Code message: ': '.... . .-.. .-.. ---'}]
+
+            # Call the function and ensure no exceptions propagate
+            with self.assertLogs() as log:
+                save_to_file(test_msg_arr)
+                self.assertIn('Error saving file', log.output[0])
