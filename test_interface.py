2cee3c6e8968f9ca40a9a5e8b36ef214bb47ae8e
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-01-12 17:27:59 +0100

    Write tests for Interface class

---
+++ test_interface.py
@@ -0,0 +1,21 @@
+import unittest
+from interface import Interface
+
+# Write tests for the interface module
+class TestInterface(unittest.TestCase):
+    def setUp(self):
+        self.interface = Interface()
+
+    def test_choose_action(self):
+        self.assertTrue(self.interface.choose_action(1))
+        self.assertTrue(self.interface.choose_action(2))
+        self.assertTrue(self.interface.choose_action(3))
+        self.assertFalse(self.interface.choose_action(0))
+        self.assertTrue(self.interface.choose_action(4))
+
+    def test_type_in_message(self):
+        self.assertEqual(self.interface._type_in_message('morse code'), 'test')
+        self.assertEqual(self.interface._type_in_message('text'), '- . ... -')
+
+if __name__ == '__main__':
+    unittest.main()
